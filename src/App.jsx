4aabfe35904349655f25ee6bde3a8001a25bf2cc
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-05-09 23:50:37 +0200

    Add scroll reset when changing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import style from "./App.module.css"
 import GlobalSpinner from "./components/GlobalSpinner/GlobalSpinner"
+import { useLayoutEffect } from "react"
 
 export function AppFallback() {
     return (
@@ -12,15 +13,28 @@ export function AppFallback() {
     )
 }
 
+const ScrollWrapper = ({ children }) => {
+    const location = useLocation()
+
+    useLayoutEffect(() => {
+        // Scroll to the top of the page when the route changes
+        window.scrollTo({ top: 0, left: 0, behavior: "instant" })
+    }, [location.pathname])
+
+    return children
+}
+
 function App() {
     return (
-        <div className={style["app"]}>
-            <Header />
-            <div className={style["content"]}>
-                <Outlet />
+        <ScrollWrapper>
+            <div className={style["app"]}>
+                <Header />
+                <div className={style["content"]}>
+                    <Outlet />
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
+        </ScrollWrapper>
     )
 }
 
