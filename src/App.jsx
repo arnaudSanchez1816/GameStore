915394d15647bb528be520a07ccab58349701c2e
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-05-14 05:33:01 +0200

    Use react scroll restoration component instead of custom one

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { Outlet, useLocation } from "react-router-dom"
+import { Outlet, ScrollRestoration } from "react-router-dom"
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import style from "./App.module.css"
 import GlobalSpinner from "./components/GlobalSpinner/GlobalSpinner"
-import { useLayoutEffect } from "react"
 
 export function AppFallback() {
     return (
@@ -13,20 +12,10 @@ export function AppFallback() {
     )
 }
 
-const ScrollWrapper = ({ children }) => {
-    const location = useLocation()
-
-    useLayoutEffect(() => {
-        // Scroll to the top of the page when the route changes
-        window.scrollTo({ top: 0, left: 0, behavior: "instant" })
-    }, [location.pathname])
-
-    return children
-}
-
 function App() {
     return (
-        <ScrollWrapper>
+        <>
+            <ScrollRestoration />
             <div className={style["app"]}>
                 <Header />
                 <div className={style["content"]}>
@@ -34,7 +23,7 @@ function App() {
                 </div>
                 <Footer />
             </div>
-        </ScrollWrapper>
+        </>
     )
 }
 
